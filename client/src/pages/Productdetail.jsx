5f2071f7fc46564c3000b8011a4ee18f43dbccca
Kishan Vyas <kishanvyas308@gmail.com>
2025-01-10 00:11:19 +0530

    [Modify] Product page | Product Detail page | Testimonials caresoul

--- client/src/pages/Productdetail.jsx
+++ client/src/pages/Productdetail.jsx
@@ -6,11 +6,9 @@ import { allProduct } from "../backend/manageProduct";
 import { loadingAtom } from "../Atoms/loadingAtom";
 import { userAtom } from "../Atoms/userAtom";
 import PopularProduct from "../Componets/PopularProduct";
-import Loading from "../Componets/Loading";
-
 import WhatsappContectButton from "../Componets/WhatsappContectButton";
 import SendRequirementButton from "../Componets/SendRequirementButton";
-import ReactImageMagnify from "react-image-magnify";
+
 
 const ProductDetail = () => {
   const [product, setProduct] = useState();
@@ -33,7 +31,7 @@ const ProductDetail = () => {
     let prod = null;
 
     if (products !== null) {
-      await products.map((p) => {
+      products.map((p) => {
         if (p.id == params.id) {
           prod = p;
         }
@@ -54,28 +52,12 @@ const ProductDetail = () => {
         <div className="max-w-4xl mt-4 mx-auto p-6 bg-white shadow-lg rounded-lg">
           <div className="flex flex-col lg:flex-row">
             <div className="flex-1">
-             <ReactImageMagnify
-                {...{
-                  smallImage: {
-                    alt: product.name,
-                    isFluidWidth: true,
-                    src: product.imageUrl,
-                  },
-                  largeImage: {
-                    src: product.imageUrl,
-                    width: 900,
-                    height: 600,
-                  },
-                  enlargedImageContainerDimensions: {
-                    width: '150%',
-                    height: '150%',
-                  },
-                  isHintEnabled: true,
-                  shouldUsePositiveSpaceLens: true,
-                }}
-               className="lg:block hidden"/>
-
-               <img src={ product.imageUrl}  className="block lg:hidden flex-1" />
+            <img
+    src={product.imageUrl}
+    alt={product.name}
+    className="w-full h-96 object-cover"  // Fixed height for the initial image
+  />
+             
             </div>
             <div className="flex-1 lg:pl-6 mt-6 lg:mt-0">
               <h1 className="text-3xl font-bold mb-4 text-gray-800">
@@ -84,56 +66,87 @@ const ProductDetail = () => {
               <table className="mb-4 mt-2 w-full border-collapse border border-gray-200">
                 <tbody>
                   <tr>
-                    <td className="border border-gray-200 px-4 py-2 font-bold">MOQ:</td>
-                    <td className="border border-gray-200 px-4 py-2">{product.moq} Piece</td>
+                    <td className="border border-gray-200 px-4 py-2 font-bold">
+                      MOQ:
+                    </td>
+                    <td className="border border-gray-200 px-4 py-2">
+                      {product.moq} Piece
+                    </td>
                   </tr>
                   <tr>
-                    <td className="border border-gray-200 px-4 py-2 font-bold">Business Type:</td>
-                    <td className="border border-gray-200 px-4 py-2">Manufacturers, Wholesaler, Supplier</td>
+                    <td className="border border-gray-200 px-4 py-2 font-bold">
+                      Business Type:
+                    </td>
+                    <td className="border border-gray-200 px-4 py-2">
+                      Manufacturers, Wholesaler, Supplier
+                    </td>
                   </tr>
                   <tr>
-                    <td className="border border-gray-200 px-4 py-2 font-bold">Country of Origin:</td>
+                    <td className="border border-gray-200 px-4 py-2 font-bold">
+                      Country of Origin:
+                    </td>
                     <td className="border border-gray-200 px-4 py-2">India</td>
                   </tr>
                   <tr>
-                    <td className="border border-gray-200 px-4 py-2 font-bold">Size:</td>
-                    <td className="border border-gray-200 px-4 py-2">{product.size}</td>
+                    <td className="border border-gray-200 px-4 py-2 font-bold">
+                      Size:
+                    </td>
+                    <td className="border border-gray-200 px-4 py-2">
+                      {product.size}
+                    </td>
                   </tr>
                   <tr>
-                    <td className="border border-gray-200 px-4 py-2 font-bold">Material:</td>
-                    <td className="border border-gray-200 px-4 py-2">{product.material}</td>
+                    <td className="border border-gray-200 px-4 py-2 font-bold">
+                      Material:
+                    </td>
+                    <td className="border border-gray-200 px-4 py-2">
+                      {product.material}
+                    </td>
                   </tr>
-                 
                 </tbody>
               </table>
               <div className="flex items-center my-2">
-                <WhatsappContectButton product={product}/>
-                <SendRequirementButton product={product}/>
-           
+                <WhatsappContectButton product={product} />
+                <SendRequirementButton product={product} />
+               
               </div>
             </div>
           </div>
           <div className="mt-6 bg-gray-100 p-6 rounded-lg shadow-inner">
-            <h2 className="text-2xl font-bold mb-4 text-gray-800">Product Details</h2>
+            <h2 className="text-2xl font-bold mb-4 text-gray-800">
+              Product Details
+            </h2>
             <div className="grid grid-cols-2 gap-4">
               <div>
-                <p><strong>Shape:</strong> {product.details.shape ? product.details.shape : '----'}</p>
+                <p>
+                  <strong>Shape:</strong>{" "}
+                  {product.details.shape ? product.details.shape : "----"}
+                </p>
               </div>
               <div>
-                <p><strong>Color:</strong> {product.details.color ? product.details.color :'----'}</p>
+                <p>
+                  <strong>Color:</strong>{" "}
+                  {product.details.color ? product.details.color : "----"}
+                </p>
               </div>
               <div>
-                <p><strong>Pattern:</strong> {product.details.pattern ? product.details.pattern:'----'}</p>
+                <p>
+                  <strong>Pattern:</strong>{" "}
+                  {product.details.pattern ? product.details.pattern : "----"}
+                </p>
               </div>
               <div>
-                <p><strong>Finish:</strong> {product.details.finish ? product.details.finish:'----'}</p>
+                <p>
+                  <strong>Finish:</strong>{" "}
+                  {product.details.finish ? product.details.finish : "----"}
+                </p>
               </div>
             </div>
           </div>
         </div>
       )}
       <h3 className="text-center mt-8 text-2xl font-semibold">View Similar</h3>
-      <PopularProduct productIdToSkip={params.id}/>
+      <PopularProduct productIdToSkip={params.id} />
       {isLoading && <Loading />}
     </>
   );
