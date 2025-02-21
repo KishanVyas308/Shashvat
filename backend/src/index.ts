0a0ca7ba08d4fdf25b3085eb65fb47c5cfcedad0
Kishan Vyas <kishanvyas308@gmail.com>
2025-02-21 23:53:46 +0530

    [Modify/Add] Add Manage Requriment Of User Part #23

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client';
 // Importing routes
 import authRoute from './routes/authRoute';
 import productRoute from './routes/productRoute';
+import requirementRoute from './routes/requirementRoute';
 import setupSwagger from './swagger';
 
 export const prisma = new PrismaClient();
@@ -18,6 +19,7 @@ app.use(cors());
 const api = '/api/v1';
 app.use(api + '/auth', authRoute);
 app.use(api + '/products', productRoute);
+app.use(api + '/requirements', requirementRoute);
 
 // Setup Swagger
 setupSwagger(app);
